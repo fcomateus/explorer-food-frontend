da31da2c71cc769b499831854b3a82bab549b929
Francisco Mateus <fmfr@outlook.com>
2023-05-04 18:34:02 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Header } from './components/Header'
+
+function App() {
+
+  return (
+        <Header>
+          Oi
+        </Header>
+  )
+}
+
+export default App
---
+++ src/styles/theme.js
@@ -0,0 +1,14 @@
+export default {
+  COLORS: {
+    HEADER: '#001119',
+    SEARCH: '#0D1D25',
+    BACKGROUND_BODY: '#000A0F',
+    BACKGROUND_CARD: '#00070A',
+    TAG_EXIBITION: '#0D1D25',
+    INPUT: '#0D161B',
+    TAG_INPUT: '#76797B',
+    RED: '#750310',
+    LIGHT_BLUE: '#82F3FF',
+    WHITE: '#E1E1E6'
+  }
+}
